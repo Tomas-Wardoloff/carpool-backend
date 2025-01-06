1ab31db4bb943eed7a7340583895eeeac22718bf
Tomas-Wardoloff <tomaswardoloff@gmail.com>
2025-01-06 11:33:29 -0300

    Rename accounts URL path to auth for django-allauth integration

--- src/carpool/urls.py
+++ src/carpool/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('accounts/', include('allauth.urls')),
+    path('auth/', include('allauth.urls')),
 ]
