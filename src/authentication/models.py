4a7132dc70ea84bbf37d42e30b83b5e45414b59d
Tomas-Wardoloff <tomaswardoloff@gmail.com>
2025-02-12 13:13:19 -0300

    Format files

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -1,7 +1,4 @@
-from datetime import datetime
-
 from django.db import models
-from django.core.exceptions import ValidationError
 from django.contrib.auth.models import AbstractUser
 
 from phonenumber_field.modelfields import PhoneNumberField
@@ -11,8 +8,6 @@ from .managers import CustomUserManager
 
 class CustomUser(AbstractUser):
     """
-    CustomUser model representing a user in the system.
-    
     Attributes:
         - email (EmailField): The email address of the user.
         - birth_date (DateField): The birth date of the user.
@@ -20,7 +15,7 @@ class CustomUser(AbstractUser):
         - document_number (CharField): The document number of the user.
         - phone_number (PhoneNumberField): The phone number of the user.
         - profile_picture (ImageField): The profile picture of the user.
-    
+
     Attributes inherits from AbstractUser:
         - username (CharField): The username of the user.
         - password (CharField): The password of the user.
@@ -31,42 +26,40 @@ class CustomUser(AbstractUser):
         - is_active (BooleanField): Designates whether the user account is active.
         - date_joined (DateTimeField): The date and time when the user account was created.
         - last_login (DateTimeField): The date and time when the user last logged in.
-        
+
     Custom Manager:
         - objects (CustomUserManager): Custom manager for the CustomUser model.
-    
+
     Methods:
         - __str__: Returns a string representation of the user.
-        - clean: Validates the document number and birth date of the user.
-        - save: Overrides the save method to set the username as 'first_name last_name' when saving the user.
+        - save: Capitalizes the first and last name of the user and assigns the username.
     """
-    email = models.EmailField(verbose_name='Email', unique=True)
-    birth_date = models.DateField(verbose_name='Fecha de nacimiento', blank=True, null=True)
-    about_me = models.TextField(max_length=500, blank=True, verbose_name='Descripcion')
-    document_number = models.CharField(max_length=8, verbose_name='Número de documento')
-    phone_number = PhoneNumberField(region='AR', verbose_name='Número de teléfono')
+
+    email = models.EmailField(verbose_name="Email", unique=True)
+    birth_date = models.DateField(
+        verbose_name="Fecha de nacimiento", blank=True, null=True
+    )
+    about_me = models.TextField(max_length=500, blank=True, verbose_name="Descripcion")
+    document_number = models.CharField(max_length=8, verbose_name="Número de documento")
+    phone_number = PhoneNumberField(region="AR", verbose_name="Número de teléfono")
     username = models.CharField(max_length=150, unique=False, blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
-    
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
-    
+    profile_picture = models.ImageField(
+        upload_to="profile_pictures/", blank=True, null=True
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
     objects = CustomUserManager()
-    
+
     def __str__(self) -> str:
         return self.email
-    
+
     def save(self, *args, **kwargs):
         self.first_name = self.first_name.capitalize()
         self.last_name = self.last_name.capitalize()
-        self.username = f"{self.first_name} {self.last_name}" # auto assign username
-        
+        self.username = f"{self.first_name} {self.last_name}"  # auto assign username
+
         if self.profile_picture:
             self.profile_picture.name = f"{self.email}_profile_picture.{self.profile_picture.name.split('.')[-1]}"
         super().save(*args, **kwargs)
-    
-    def get_user_rating(self) -> float | str:
-        reviews = self.reviews.all()
-        if len(reviews) > 20:
-            return round(sum([review.rating for review in reviews]) / len(reviews), 1)
-        return 'Este usuario no tiene suficientes calificaciones'
--- src/trip/models.py
+++ src/trip/models.py
@@ -5,102 +5,116 @@ from authentication.models import CustomUser
 
 class State(models.Model):
     """
-    State model representing a state in the system.
-    
     Attributes:
         - name (CharField): The name of the state.
         - abbreviation (CharField): The abbreviation of the state.
         - country (CharField): The country of the state.
-    
+
     Attributes inherits from Model:
         - id (AutoField): The primary key for the state.
-        
+
     Methods:
         - save: Capitalizes the name and country of the state before saving it.
         - __str__: Returns a string representation of the state.
-    
+
     Meta:
         - unique_together: The name, abbreviation and country of the state must be unique together.
     """
+
     name = models.CharField(max_length=100, verbose_name="Nombre")
     abbreviation = models.CharField(max_length=2, verbose_name="Abreviatura")
     country = models.CharField(max_length=100, verbose_name="País")
-    
+
     def __str__(self):
         return f"{self.name}, {self.country}"
-    
+
     def save(self, *args, **kwargs):
         self.name = self.name.title()
         self.country = self.country.title()
         self.abbreviation = self.abbreviation.upper()
-        
+
         self.full_clean()
         super().save(*args, **kwargs)
-        
+
     class Meta:
-        unique_together = ('name', 'abbreviation', 'country')
+        unique_together = ("name", "abbreviation", "country")
 
 
 class City(models.Model):
     """
-    City model representing a city in the system.
-    
     Attributes:
         - name (CharField): The name of the city.
         - latitude (FloatField): The latitude of the city.
         - longitude (FloatField): The longitude of the city.
         - state (CharField): The state of the city.
-    
+
     Attributes inherits from Model:
         - id (AutoField): The primary key for the city.
-    
+
     Methods:
         - save: Capitalizes the name of the city before saving it.
         - __str__: Returns a string representation of the city.
     """
-    name = models.CharField(max_length=100, verbose_name='Nombre')
-    latitude = models.FloatField(verbose_name='Latitud')
-    longitude = models.FloatField(verbose_name='Longitud')
-    state = models.ForeignKey(State, related_name='cities', on_delete=models.CASCADE, verbose_name='Provincia')
-    
+
+    name = models.CharField(max_length=100, verbose_name="Nombre")
+    latitude = models.FloatField(verbose_name="Latitud")
+    longitude = models.FloatField(verbose_name="Longitud")
+    state = models.ForeignKey(
+        State, related_name="cities", on_delete=models.CASCADE, verbose_name="Provincia"
+    )
+
+    def __str__(self):
+        return f"{self.name}, {self.state}"
+
     def save(self, *args, **kwargs):
         self.name = self.name.title()
         self.full_clean()
         super().save(*args, **kwargs)
 
-    def __str__(self):
-        return f"{self.name}, {self.state}"    
-
 
 class Vehicle(models.Model):
     """
-    Vehicle model representing a vehicle in the system.
-    
     Attributes:
         - owner (ForeignKey): The owner of the vehicle.
         - license_plate (CharField): The license plate of the vehicle.
         - brand (CharField): The brand of the vehicle.
         - model (CharField): The model of the vehicle.
-        
+
     Attributes inherits from Model:
         - id (AutoField): The primary key for the vehicle.
-        
-    Methods:    
+
+    Methods:
         - __str__: Returns a string representation of the vehicle.
+
+    Meta:
+        - unique_together: The license plate and owner of the vehicle must be unique together.
     """
-    owner = models.ForeignKey(CustomUser, related_name='vehicles', on_delete=models.CASCADE, verbose_name='Propietario')
-    license_plate = models.CharField(max_length=7, verbose_name='Patente', unique=True)
-    brand = models.CharField(max_length=50, verbose_name='Marca')
-    model = models.CharField(max_length=50, verbose_name='Modelo')
-    
+
+    owner = models.ForeignKey(
+        CustomUser,
+        related_name="vehicles",
+        on_delete=models.CASCADE,
+        verbose_name="Propietario",
+    )
+    license_plate = models.CharField(max_length=7, verbose_name="Patente", unique=True)
+    brand = models.CharField(max_length=50, verbose_name="Marca")
+    model = models.CharField(max_length=50, verbose_name="Modelo")
+
     def __str__(self):
-        return f'{self.brand} {self.model} {self.license_plate}'
+        return f"{self.brand} {self.model} {self.license_plate}"
+
+    def save(self, *args, **kwargs):
+        self.brand = self.brand.title()
+        self.model = self.model.title()
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        unique_together = ("license_plate", "owner")
 
 
 class Trip(models.Model):
     """
-    Trip model representing a trip in the system.
-    
     Attributes:
         - origin_city (ForeignKey): The origin city of the trip.
         - destination_city (ForeignKey): The destination city of the trip.
@@ -112,86 +126,150 @@ class Trip(models.Model):
         - vehicle (ForeignKey): The vehicle of the trip.
         - participants (ManyToManyField): The participants of the trip.
         - creator (ForeignKey): The creator of the trip.
-    
+
     Attributes inherits from Model:
         - id (AutoField): The primary key for the trip.
-        
+
     Methods:
         - __str__: Returns a string representation of the trip.
     """
-    origin_city = models.ForeignKey(City, related_name='trips_from', on_delete=models.CASCADE, verbose_name='Ciudad de origen') 
-    destination_city = models.ForeignKey(City, related_name='trips_to', on_delete=models.CASCADE, verbose_name='Ciudad de destino') 
-    departure_date = models.DateField(verbose_name='Fecha de salida')
-    departure_time = models.TimeField(verbose_name='Hora de salida')
-    pet_allowed = models.BooleanField(default=False, verbose_name='Se permiten mascotas')
-    smoking_allowed = models.BooleanField(default=False, verbose_name='Se permite fumar')
-    kids_allowed = models.BooleanField(default=False, verbose_name='Se permiten niños')
-    vehicle = models.ForeignKey(Vehicle, on_delete=models.SET_NULL, null=True, verbose_name='Vehículo')
-    participants = models.ManyToManyField(CustomUser, related_name='trips', through='TripParticipant', verbose_name='Participantes')
-    creator = models.ForeignKey(CustomUser, related_name='created_trips', on_delete=models.CASCADE, verbose_name='Creador')
+
+    origin_city = models.ForeignKey(
+        City,
+        related_name="trips_from",
+        on_delete=models.CASCADE,
+        verbose_name="Ciudad de origen",
+    )
+    destination_city = models.ForeignKey(
+        City,
+        related_name="trips_to",
+        on_delete=models.CASCADE,
+        verbose_name="Ciudad de destino",
+    )
+    departure_date = models.DateField(verbose_name="Fecha de salida")
+    departure_time = models.TimeField(verbose_name="Hora de salida")
+    pet_allowed = models.BooleanField(
+        default=False, verbose_name="Se permiten mascotas"
+    )
+    smoking_allowed = models.BooleanField(
+        default=False, verbose_name="Se permite fumar"
+    )
+    kids_allowed = models.BooleanField(default=False, verbose_name="Se permiten niños")
+    vehicle = models.ForeignKey(
+        Vehicle, on_delete=models.SET_NULL, null=True, verbose_name="Vehículo"
+    )
+    participants = models.ManyToManyField(
+        CustomUser,
+        related_name="trips",
+        through="TripParticipant",
+        verbose_name="Participantes",
+    )
+    creator = models.ForeignKey(
+        CustomUser,
+        related_name="created_trips",
+        on_delete=models.CASCADE,
+        verbose_name="Creador",
+    )
 
     def __str__(self):
-        return f'from {self.origin_city} to {self.destination_city} on {self.departure_date}'
-    
+        return f"from {self.origin_city} to {self.destination_city} on {self.departure_date}"
+
 
 class TripParticipant(models.Model):
     """
-    TripParticipant model representing a participant in a trip in the system.
-    
     Attributes:
         - user (ForeignKey): The user of the trip.
         - trip (ForeignKey): The trip.
         - role (CharField): The role of the user on the trip.
-    
+
     Attributes inherits from Model:
         - id (AutoField): The primary key for the participant.
-    
+
     Methods:
         - __str__: Returns a string representation of the participant.
-    
+
     Meta:
-        - unique_together: The user and trip of the participant must be unique together.
+        - unique_together: The user and trip of the model must be unique together.
     """
-    user = models.ForeignKey(CustomUser, related_name='user_trips',on_delete=models.CASCADE, verbose_name='Usuario')
-    trip = models.ForeignKey(Trip, related_name='trip_participants', on_delete=models.CASCADE, verbose_name='Viaje') 
-    role = models.CharField(max_length=10, choices=[('driver', 'Driver'),('passenger', 'Passenger'),], verbose_name='Rol')
-    
+
+    user = models.ForeignKey(
+        CustomUser,
+        related_name="user_trips",
+        on_delete=models.CASCADE,
+        verbose_name="Usuario",
+    )
+    trip = models.ForeignKey(
+        Trip,
+        related_name="trip_participants",
+        on_delete=models.CASCADE,
+        verbose_name="Viaje",
+    )
+    role = models.CharField(
+        max_length=10,
+        choices=[
+            ("driver", "Driver"),
+            ("passenger", "Passenger"),
+        ],
+        verbose_name="Rol",
+    )
+
     def __str__(self):
         return f"{self.user.email} as {self.role} in trip {self.trip}"
-    
+
     class Meta:
-        unique_together = ('user', 'trip')
-        
+        unique_together = ("user", "trip")
+
 
 class TripJoinRequest(models.Model):
     """
-    TripJoinRequest model representing a request to join a trip.
-    
     Attributes:
         - user (ForeignKey): The user of the request.
         - trip (ForeignKey): The trip of the request.
         - created_at (DateTimeField): The creation date of the request.
         - updated_at (DateTimeField): The update date of the request.
         - status (CharField): The status of the request.
-    
+
     Attributes inherits from Model:
         - id (AutoField): The primary key for the request.
-        
+
     Methods:
         - __str__: Returns a string representation of the request.
-    
+
     Meta:
         - unique_together: The user and trip of the request must be unique together.
     """
-    user = models.ForeignKey(CustomUser, related_name='join_requests', on_delete=models.CASCADE, verbose_name='Usuario')
-    trip = models.ForeignKey(Trip, related_name='join_requests', on_delete=models.CASCADE, verbose_name='Viaje')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Fecha de actualización')
-    status = models.CharField(max_length=10, choices=[('pending', 'Pending'),('accepted', 'Accepted'),('rejected', 'Rejected')], verbose_name='Estado', default='pending')
-    
+
+    user = models.ForeignKey(
+        CustomUser,
+        related_name="join_requests",
+        on_delete=models.CASCADE,
+        verbose_name="Usuario",
+    )
+    trip = models.ForeignKey(
+        Trip,
+        related_name="join_requests",
+        on_delete=models.CASCADE,
+        verbose_name="Viaje",
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Fecha de creación"
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Fecha de actualización"
+    )
+    status = models.CharField(
+        max_length=10,
+        choices=[
+            ("pending", "Pending"),
+            ("accepted", "Accepted"),
+            ("rejected", "Rejected"),
+        ],
+        verbose_name="Estado",
+        default="pending",
+    )
+
     def __str__(self):
         return f"{self.user.email} request to join trip {self.trip}"
-    
+
     class Meta:
-        unique_together = ('user', 'trip')
-        
+        unique_together = ("user", "trip")
